1a2b47a8424f44e29e0629773b0179f15b0612fe
CaydenFallin <caydenfallin@gmail.com>
2026-04-29 20:28:24 -0400

    removed email functionality due to costs

--- core/views.py
+++ core/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect
-from django.core.mail import send_mail
 from .forms import ContactForm
 from projects.models import Project
-import os
 
 def index(request):
     projects = Project.objects.prefetch_related('images', 'links').all()
@@ -12,16 +10,6 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            msg = form.save()
-            try:
-                send_mail(
-                    subject=f'Portfolio message from {msg.name}',
-                    message=f'Name: {msg.name}\nEmail: {msg.email}\n\nMessage:\n{msg.message}',
-                    from_email=msg.email,
-                    recipient_list=[os.getenv('EMAIL_HOST_USER')],
-                    fail_silently=False,
-                )
-            except Exception:
-                pass
+            form.save()
             return redirect('index')
     return redirect('index')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,14 +134,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
-
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
