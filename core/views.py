f50162f66a588e957f76966d4e65dea2e221c97a
CaydenFallin <caydenfallin@gmail.com>
2026-04-29 20:23:52 -0400

    email wrapping

--- core/views.py
+++ core/views.py
@@ -13,12 +13,15 @@ def contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             msg = form.save()
-            send_mail(
-                subject=f'Portfolio message from {msg.name}',
-                message=f'Name: {msg.name}\nEmail: {msg.email}\n\nMessage:\n{msg.message}',
-                from_email=msg.email,
-                recipient_list=[os.getenv('EMAIL_HOST_USER')],
-                fail_silently=True,
-            )
+            try:
+                send_mail(
+                    subject=f'Portfolio message from {msg.name}',
+                    message=f'Name: {msg.name}\nEmail: {msg.email}\n\nMessage:\n{msg.message}',
+                    from_email=msg.email,
+                    recipient_list=[os.getenv('EMAIL_HOST_USER')],
+                    fail_silently=False,
+                )
+            except Exception:
+                pass
             return redirect('index')
     return redirect('index')
